63316d7bf6eddfbd7ba2f1723e2a978726ea3f94
Dmytro Martynenko <DoopsDM@gmail.com>
2023-04-30 14:49:32 +0300

    create app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import Feedback from './FeedbackOptions/feedback';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Feedback />
     </div>
   );
 };
